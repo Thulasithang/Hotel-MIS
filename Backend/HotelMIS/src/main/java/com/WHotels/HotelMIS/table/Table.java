9a878b8a7f3985eac97e1e0f249b08c38a16f8a9
ThisuraGallage <tggallage1@gmail.com>
2023-09-05 10:38:23 +0530

    Backend added

---
+++ Backend/HotelMIS/src/main/java/com/WHotels/HotelMIS/table/Table.java
@@ -0,0 +1,70 @@
+package com.WHotels.HotelMIS.table;
+import jakarta.persistence.*;
+
+@Entity
+@jakarta.persistence.Table(name="restaurant_table")
+public class Table {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="table_id")
+    private long id;
+
+    @Column(name="occupied")
+    private boolean occupied;
+    @Column(name="waiter_requested")
+    private boolean waiterRequested;
+    @Transient
+    private boolean reserved=false;
+
+    public Table(long id) {
+        this.id = id;
+    }
+
+    public Table() {
+    }
+
+    public Table(boolean occupied, boolean waiterRequested) {
+        this.occupied = occupied;
+        this.waiterRequested = waiterRequested;
+        //this.reserved = reserved;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean isWaiterRequested() {
+        return waiterRequested;
+    }
+
+    public void setWaiterRequested(boolean waiterRequested) {
+        this.waiterRequested = waiterRequested;
+    }
+
+    public void setReserved(boolean reserved){
+        this.reserved=reserved;
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "id=" + id +
+                ", occupied=" + occupied +
+                ", waiterRequested=" + waiterRequested +
+                ", reserved=" + reserved +
+                '}';
+    }
+}
+
---
+++ Backend/HotelMIS/src/test/java/com/WHotels/HotelMIS/HotelMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WHotels.HotelMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
