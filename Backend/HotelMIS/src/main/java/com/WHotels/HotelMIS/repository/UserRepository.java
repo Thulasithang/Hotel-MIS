73a37a30b0a28740533ee2404cb08ee73d83204b
therazus <74174794+therazus@users.noreply.github.com>
2023-10-01 12:16:32 +0530

    Deleted backend

--- Backend/HotelMIS/src/main/java/com/WHotels/HotelMIS/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.WHotels.HotelMIS.repository;
-
-import com.WHotels.HotelMIS.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<Users,Long> {
-    Optional<Users> findByUsername(String username);
-}
